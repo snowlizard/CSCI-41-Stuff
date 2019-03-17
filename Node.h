bd14f26113b3a3258c3fe21ae1f5eae5603198f9
snowlizard <illegal_burrito@live.com>
2019-03-17 13:08:30 -0700

    added Node.h

---
+++ Node.h
@@ -0,0 +1,48 @@
+#ifndef _NODE
+#define _NODE
+template<class ItemType>
+class Node
+{
+private:
+    ItemType       item;
+    Node<ItemType> *next;
+public:
+    Node();  //default constructor
+    Node(const ItemType &anItem, Node<ItemType> *nextPtr); //none default constructor
+    void setItem(const ItemType &anItem);
+    void setNext(Node<ItemType> *nextPtr);
+    ItemType getItem() const;
+    Node<ItemType> *getNext() const;
+};
+template<class ItemType>
+Node<ItemType>::Node()
+{
+    next = NULL;
+}
+template<class ItemType>
+Node<ItemType>::Node(const ItemType &anItem, Node<ItemType> *nextPtr)
+{
+    item = anItem;
+    next = nextPtr;
+}
+template<class ItemType>
+void Node<ItemType>::setItem(const ItemType &anItem)
+{
+    item = anItem;
+}
+template<class ItemType>
+void Node<ItemType>::setNext(Node<ItemType> *nextPtr)
+{
+    next = nextPtr;
+}
+template<class ItemType>
+ItemType Node<ItemType>::getItem() const
+{
+    return item;
+}
+template<class ItemType>
+Node<ItemType>* Node<ItemType>::getNext() const
+{
+    return next;
+}
+#endif
