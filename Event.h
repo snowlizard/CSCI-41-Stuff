ff572315fe771704c6e0267b04ce1a169c4cb9f1
snowlizard <illegal_burrito@live.com>
2019-03-26 14:03:55 -0700

    Just need to work on overloading functions

--- Event.h
+++ Event.h
@@ -8,7 +8,7 @@ private:
     int occurTime;
 public:
     Event();
-    Event(char eStatue, int oTime);
+    Event(char eState, int oTime);
     void setEventStatus(char eStatus);
     char getEventStatus();
     void setOccurTime(int oTime);
@@ -18,6 +18,40 @@ public:
     bool operator==(const Event &anEvent) const;
     friend ostream &operator<<(ostream &out, Event &anEvent);
 };
+
+Event::Event()
+{
+    
+}
+
+Event::Event( char eState, int oTime )
+{
+    eventStatus = eState;
+    occurTime  = oTime;
+}
+
+void Event::setEventStatus( char eStatus )
+{
+    eventStatus = eStatus;
+}
+
+char Event::getEventStatus()
+{
+    return eventStatus;
+}
+
+void Event::setOccurTime( int oTime )
+{
+    occurTime = oTime;
+}
+
+int Event::getOccurTime()
+{
+    return occurTime;
+}
+//==================== Operator Overloading =======
+
+
 
 
 #endif 
